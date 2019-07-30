9e0f47a5ccf4c24e9592cb72e98f2afe0b3997e8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-30 16:13:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,5 +7,5 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language)
-  puts "Hi, my name is Edwin and I am learning to program in Ruby.\n"
+  puts "Hi, my name is #{name} and I am learning to program in Ruby.\n"
 end
